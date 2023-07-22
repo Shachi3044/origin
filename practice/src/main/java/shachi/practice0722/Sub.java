6583ff0988f43f4b4c7fa893cef933e69cbd7480
Shachi3044 <122660448+Shachi3044@users.noreply.github.com>
2023-07-22 18:02:46 +0900

    0722 成果

---
+++ practice/src/main/java/shachi/practice0722/Sub.java
@@ -0,0 +1,13 @@
+package shachi.practice0722;
+
+public class Sub extends Super {
+	public Sub() {
+		super("B");
+		System.out.println("C");
+	}
+	public Sub(String val) {
+		this();
+		System.out.println(val);
+	}
+
+}
---
+++ practice/src/main/java/shachi/practice0722/Super.java
@@ -0,0 +1,12 @@
+package shachi.practice0722;
+
+public class Super {
+	public Super() {
+		System.out.println("A");
+	}
+	public Super(String val) {
+		this();
+		System.out.println(val);
+	}
+
+}
